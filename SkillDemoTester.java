c07bb3db17b8b4b362b52992583de7ddc0a69ad5
msioson <97692924+msioson@users.noreply.github.com>
2022-02-04 16:47:31 -0800

    Update SkillDemoTester.java

--- SkillDemoTester.java
+++ SkillDemoTester.java
@@ -4,6 +4,9 @@ import org.junit.*;
 public class SkillDemoTester {
     @Test
     public void testRemainder() {
-        assertEquals(2, SkillDemo.remainder(3, 2));
+        assertEquals(1, SkillDemo.remainder(3, 2));
     }
 }
+
+// javac -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" SkillDemoTester.java
+// java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore SkillDemoTester
